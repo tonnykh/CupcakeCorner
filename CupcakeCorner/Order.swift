82f53f5cda336ff6e06415f6e37c60dee33a4175
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-27 18:18:31 +0530

    Add an Order class to store all data

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,19 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Khumbongmayum Tonny on 27/06/23.
+//
+
+import Foundation
+
+class Order: ObservableObject {
+    static let types: [String] = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type: Int = 0
+    @Published var quantity: Int = 3
+    
+    @Published var specialRequestEnabled: Bool = false
+    @Published var extraFrosting: Bool = false
+    @Published var addSprinkles: Bool = false
+}

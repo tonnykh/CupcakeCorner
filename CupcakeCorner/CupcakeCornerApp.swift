72b03943f1aa8b5aefcbd95fb3049adbc6b100cf
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-27 15:34:24 +0530

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Khumbongmayum Tonny on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

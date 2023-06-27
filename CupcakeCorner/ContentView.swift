451ebfd279365dce1ebcf9f53ddba01dc473fac1
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-27 18:43:49 +0530

    Add basic UI

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var order: Order = Order()
     
     var body: some View {
-        Text("Hello")
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id:\.self) { index in
+                            Text(Order.types[index])
+                        }
+                    }
+                }
+                
+                Section {
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity,
+                        in: 1...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+            }
+            .navigationTitle("Cupcake Corner")
+        }
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -13,7 +13,15 @@ class Order: ObservableObject {
     @Published var type: Int = 0
     @Published var quantity: Int = 3
     
-    @Published var specialRequestEnabled: Bool = false
+    @Published var specialRequestEnabled: Bool = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
     @Published var extraFrosting: Bool = false
     @Published var addSprinkles: Bool = false
 }
